2a8f1e8d65c03ad7185d2cb99cecd93a98bd3004
Trolle <trolle.geuna@gmail.com>
2015-11-10 17:22:57 +0100

    fixing and trixing

--- s1.py
+++ s1.py
@@ -9,7 +9,7 @@ def dna():          # uppgift 1
     return "^[ACGT]+$"
 
 def sorted():       # uppgift 2
-    return "(^0*1*2*3*4*5*6*7*8*9*$)"
+    return "^9*8*7*6*5*4*3*2*1*0*$"
 
 def hidden1(x):     # uppgift 3
 # indata x är strängen som vi vill konstruera ett regex för att söka efter
@@ -26,7 +26,9 @@ def equation():     # uppgift 5
     return "^(\+|\-)?((\d)+[\+\-\*\/])*(\d)+(=((\d)+[\+\-\*\/])*(\d)+)?$"
 
 def parentheses():  # uppgift 6
-    return ""
+    return "^(\((\((\((\((\(\))*\))*\))*\))*\))*$"
+    #    ^(\( \) )*$
+    # (\(        ( \( \) )           *\) )
 
 def sorted3():      # uppgift 7
     return ""
